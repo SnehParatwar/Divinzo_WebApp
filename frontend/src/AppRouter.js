ca68eb8e0b233fb8db4912057af9602b9c2f689e
Sneh Navin Paratwar <125972532+SnehParatwar@users.noreply.github.com>
2024-06-14 15:15:16 +0530

    Header is made (inital) Routing proper has to be done

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Home from './components/Home';
 // import About from './components/About';
+import Header from './components/header/header';
 
 const AppRouter = () => (
   <Router>
     <Routes>
-      {/* <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} /> */}
+      
+      <Route path="/" element={<Header />} />
     </Routes>
   </Router>
 );
---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css"
+
+import Header from "../../components/header/header";
+
+const home =()=>{
+    return(
+        <>
+        </>
+    )
+}
